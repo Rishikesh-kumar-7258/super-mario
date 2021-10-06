8bbdbad54a51302efdead6308cea3b56baacb7a0
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-10-06 23:29:55 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.constants import QUIT
+from pygame.color import THECOLORS
+
+pygame.init()
+
+
+window_width = 1000
+window_height = 500
+
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Super Mario")
+
+clock = pygame.time.Clock()
+
+while 1:
+
+    events = pygame.event.get()
+    event_list = []
+    for event in events:
+        if event.type == QUIT:
+            event_list.append(event)
+            exit()
+    
+    screen.fill(THECOLORS['skyblue'])
+    pygame.display.flip()
+    clock.tick(60)
